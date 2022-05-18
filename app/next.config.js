7982ebc61070fb78bc48be48484f10a77bdb24cb
karinamzalez <karinamzalez@gmail.com>
2022-05-18 11:13:54 -0500

    next.config.js: setup i18n locale for internationalization

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  i18n: {
+    // TODO: implement i18n internationalization
+    locales: ['en-US', 'es-ES'],
+    defaultLocale: 'en-US',
+    localeSubpaths: {
+      es: "es"
+    },
+  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
