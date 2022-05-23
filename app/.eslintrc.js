12bfa31b5355cf9ed0cf477ff7cd72333248c37c
karinamzalez <karinamzalez@gmail.com>
2022-05-22 23:45:59 -0500

    add eslintrc.js

---
+++ app/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'nava',
+    'prettier', 
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error'
+  },
+};
