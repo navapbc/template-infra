289e125a2d9dfd9b1a550bd7678632e599044374
Karina Munoz Gonzalez <karinagonzalez@navapbc.com>
2022-05-25 16:22:39 -0500

    Karina/wmdp 61 initial nextjs setup (#4)
    * yarn create next-app --typescript
    
    * clean up auto-generated pages, public and styles
    
    * next.config.js: setup i18n locale for internationalization
    
    * next.config.js: add code comment re: internationalization
    
    * add html tags for VO
    
    * Update app/styles/Home.module.css
    
    update stickcy nav
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * Update app/README.md
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * Update app/README.md
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * tsconfig: target es6
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>

---
+++ app/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    // TODO: implement i18n internationalization-- look into nextJS subpath routing
+    locales: ['en-US', 'es-ES'],
+    defaultLocale: 'en-US',
+    localeSubpaths: {
+      es: "es"
+    },
+  },
+  reactStrictMode: true
+}
+
+module.exports = nextConfig
