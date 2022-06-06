74bd68e76ccaca8bc778d8673a6ce90441959a1d
Karina Munoz Gonzalez <karinagonzalez@navapbc.com>
2022-06-06 11:03:40 -0500

    Karina/wmdp 35 setup test suite (#5)
    * yarn create next-app --typescript
    
    * clean up auto-generated pages, public and styles
    
    * next.config.js: setup i18n locale for internationalization
    
    * next.config.js: add code comment re: internationalization
    
    * add html tags for VO
    
    * Update app/styles/Home.module.css
    
    update stickcy nav
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * Update app/README.md
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * Update app/README.md
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * tsconfig: target es6
    
    * add babelec
    
    * add __mocks__ with mock styles for testing
    
    * -test: add first jest test and snapshot test for Home page
    - add jest.setup.js to require jest-dom
    
    * - add jest.config.js
    - package.json: add react testing library, babel and jest dependencies
    - add first test
    - update jsx
    - yarn.lock updated with new packages
    
    * Update app/.babelrc
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * Update app/package.json
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>
    
    * add testRegex and testPathIgnorePatterns
    
    * setup jest-axe
    
    * add new lines add end of files
    
    * jest.config.js: add inline comments
    
    * README: add tsconfig documentation
    
    * README: add test script documentation
    
    Co-authored-by: Rocket <rocketnova@users.noreply.github.com>

---
+++ app/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js", // sets up routing to mocks style sheet
+    "^@pages(.*)$": "<rootDir>/pages$1" //allows module imports of page components
+  },
+  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.js"],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  }, //transfrom typescript files to common js for jest compiler
+  transformIgnorePatterns: [
+    "/.next/",
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+  ],
+  testRegex: "(/test/.*(test|spec))\\.[jt]sx?$"
+};
---
+++ app/test/pages/index.test.tsx
@@ -0,0 +1,25 @@
+// test/pages/index.test.js
+
+import { render, screen } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
+import Home from "@pages/index";
+
+describe("Home", () => {
+  it("should render the heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByText(
+      /Next.js Template!/i
+    );
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toMatchSnapshot();
+  });
+
+  it("should pass accessibility scan", async () => {
+    const { container } = render(<Home />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  })
+});
