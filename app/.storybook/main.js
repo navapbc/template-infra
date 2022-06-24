3915a931fc45acc922781936a6c49c679c8f3f43
Rocket ! <rocketnova@users.noreply.github.com>
2022-06-24 01:39:06 -0700

    Clean up unnecessary WIP stuff.

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
@@ -9,18 +7,9 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-scss',
   ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
-  },
-  // // add a config.resolve.roots
-  // // try this: https://github.com/storybookjs/storybook/issues/12844
-  // webpackFinal: (config) => {
-  //   // config.resolve.roots = [ path.resolve(__dirname, '../public'), 'node_modules', ];
-  //   config.context = path.resolve(__dirname, '..');
-
-  //   return config;
-  // }
+  }
 }
