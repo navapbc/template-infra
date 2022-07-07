3b758f61ce8bb4fa5df921fada1df2dc81648ef3
karinamzalez <karinamzalez@gmail.com>
2022-07-07 16:59:05 -0500

    app.tsx: add next-intl local/messages provider for internationalization

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,27 @@
-import '../public/css/styles.css'
+import '../public/uswds/css/styles.css'
 import type { AppProps } from 'next/app'
-import { NextIntlProvider } from 'next-intl'
+import { AbstractIntlMessages, NextIntlProvider } from 'next-intl'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <NextIntlProvider messages={en}>
+  const { locale } = useRouter()
+  const [ localeMessages, setLocaleMessages ] = useState<undefined | AbstractIntlMessages>(undefined);
+
+  useEffect(() => {
+    const _getLocaleMessages = async (locale: string | undefined): Promise<void> => {
+      const messages: undefined | AbstractIntlMessages = await import(`../messages/${locale}.json`)
+      setLocaleMessages(messages)
+    }
+
+    _getLocaleMessages(locale).catch(console.error)
+  }, [])
+
+  return localeMessages && (
+    <NextIntlProvider messages={localeMessages}>
       <Component {...pageProps} />    
     </NextIntlProvider>
-  ) 
-  
+  )
 }
 
 export default MyApp
