8633709f5a9f6deaefba77e83ef2ff5d794774ee
karinamzalez <karinamzalez@gmail.com>
2022-07-07 16:57:05 -0500

    .storybook/preview: set up next-intl with storybook

--- app/.storybook/preview.js
+++ app/.storybook/preview.js
@@ -1,4 +1,14 @@
-import '../public/css/styles.css' // Apply global styling to our stories
+import '../public/uswds/css/styles.css' // Apply global styling to our stories
+import { NextIntlProvider } from "next-intl"
+import en from '../messages/en.json'
+
+export const decorators = [
+  (Story) => (
+    <NextIntlProvider locale="en" messages={en}>
+      <Story />
+    </NextIntlProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
