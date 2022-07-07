f12ffc31c7d9bd9571ae11648d86475cf5a31da9
karinamzalez <karinamzalez@gmail.com>
2022-07-07 16:57:52 -0500

    next.config: update next local config for itnernationalization

--- app/next.config.js
+++ app/next.config.js
@@ -2,8 +2,9 @@
 
 const nextConfig = {
   i18n: {
-    locales: ['en-US', 'es-ES'],
-    defaultLocale: 'en-US'
+    // NOTE: NextJS will automatically detect with local the user preders based on the 'Accept-Language' header and the current domain. User will be redirected to detected locale's subpath -- i.e. 'example.com/es'
+    locales: ['en', 'es'],
+    defaultLocale: 'en'
   },
   reactStrictMode: true,
 }
