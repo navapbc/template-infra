e2901f1afaf9a9540a4aefe806a2c15f7c6cd701
karinamzalez <karinamzalez@gmail.com>
2022-07-13 22:03:32 -0500

    add components dir and Layout component

---
+++ app/src/components/Layout.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from 'next-i18next'
+import { ReactElement } from 'react'
+
+type Props = {
+  children: ReactElement
+}
+
+const Layout = ({ children }: Props): JSX.Element => {
+  const {t} = useTranslation('common')
+
+  return (
+    <div className="container">
+      <header className="header">
+        <em>{t('Layout.header')}</em>
+      </header>
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <em>{t('Layout.footer')}</em>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
