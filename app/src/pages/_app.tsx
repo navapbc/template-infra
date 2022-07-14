d7da65c6aee044380f43131e13e65b5fa998e854
karinamzalez <karinamzalez@gmail.com>
2022-07-13 22:02:02 -0500

    _app.tsx: add Layout to app

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../../styles/styles.scss'
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default appWithTranslation(MyApp)
