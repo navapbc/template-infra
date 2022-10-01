bdd774d626e6179d2fd74a06827931290b459114
Loren Yu <yuloren@gmail.com>
2022-10-01 06:43:02 -0700

    Edit files in place

--- bin/bootstrap-account.sh
+++ bin/bootstrap-account.sh
@@ -6,23 +6,31 @@ PROJECT_NAME=$(basename $(PWD))
 
 cd infra/bootstrap/account
 
-cat main.tf \
-  | sed "s/<PROJECT_NAME>/$PROJECT_NAME/" \
-  > tmp.tf
-mv tmp.tf main.tf
-
+# Initialize terraform
 terraform init
 
+# First replace the placeholder value for <PROJECT_NAME> in main.tf
+# The project name is used to define unique names for the infrastructure
+# resources that are created in the subsequent steps.
+sed -i .bak "s/<PROJECT_NAME>/$PROJECT_NAME/" main.tf
+
+# Create the infrastructure for the terraform backend such as the S3 bucket
+# for storing tfstate files and the DynamoDB table for tfstate locks.
 terraform apply -auto-approve
 
+# Get the name of the S3 bucket that was created to store the tf state
+# and the name of the DynamoDB table that was created for tf state locks.
+# This will be used to configure the S3 backend in main.tf
 TF_STATE_BUCKET_NAME=$(terraform output -raw tf_state_bucket_name)
 TF_LOCKS_TABLE_NAME=$(terraform output -raw tf_locks_table_name)
 
-cat main.tf \
-  | sed "s/<TF_STATE_BUCKET_NAME>/$TF_STATE_BUCKET_NAME/" \
-  | sed "s/<TF_LOCKS_TABLE_NAME>/$TF_LOCKS_TABLE_NAME/" \
-  | sed 's/#uncomment# //g' \
-  > tmp.tf
-mv tmp.tf main.tf
+# Configure the S3 backend in main.tf by replacing the placeholder
+# values with the actual values from the previous step, then
+# uncomment the S3 backend block
+sed -i .bak "s/<TF_STATE_BUCKET_NAME>/$TF_STATE_BUCKET_NAME/" main.tf
+sed -i .bak "s/<TF_LOCKS_TABLE_NAME>/$TF_LOCKS_TABLE_NAME/" main.tf
+sed -i .bak 's/#uncomment# //g' main.tf
 
+# Re-initialize terraform with the new backend and copy the tfstate
+# to the new backend in S3
 terraform init -force-copy
--- bin/template-only-destroy-account.sh
+++ bin/template-only-destroy-account.sh
@@ -5,22 +5,26 @@ set -euxo pipefail
 
 cd infra/bootstrap/account
 
-cat ../../modules/bootstrap/main.tf \
-  | sed 's/resource "aws_s3_bucket" "tf_state" {/&\n  force_destroy = true/' \
-  | sed 's/resource "aws_s3_bucket" "tf_log" {/&\n  force_destroy = true/' \
-  | sed 's/prevent_destroy = true/prevent_destroy = false/g' \
-  > tmp.tf
-mv tmp.tf ../../modules/bootstrap/main.tf
+# The following lines update S3 buckets in the terraform bootstrap module
+# add force_destroy = true to S3 resource blocks and update any lifecycle
+# prevent_destroy = true rules to false. The reason we need to do this is
+# because S3 buckets by default are protected from destruction to avoid
+# loss of data. See [Terraform: Destroy/Replace Buckets](https://medium.com/interleap/terraform-destroy-replace-buckets-cf9d63d0029d)
+# for a more in depth explanation.
+sed -i .bak 's/resource "aws_s3_bucket" "tf_state" {/&\n  force_destroy = true/' ../../modules/bootstrap/main.tf
+sed -i .bak 's/resource "aws_s3_bucket" "tf_log" {/&\n  force_destroy = true/' ../../modules/bootstrap/main.tf
+sed -i .bak 's/prevent_destroy = true/prevent_destroy = false/g' ../../modules/bootstrap/main.tf
 
+# Apply the S3 bucket changes from the previous step
 terraform apply -auto-approve
 
-# Delete the backend s3 block
-cat main.tf \
-  | sed '{N;N;N;N;N;N;N;N;N;N;N;N;s/backend "s3" {.*}//;}' \
-  > tmp.tf
-mv tmp.tf main.tf
+# Delete the backend S3 block to re-configure terraform to use local state
+# since we're about to delete the backend S3 bucket
+sed -i .bak '{N;N;N;N;N;N;N;N;N;N;N;N;s/backend "s3" {.*}//;}' main.tf
 
-# Unconfigure S3 backend and move tfstate file to local tfstate
+# Now re-initialize terraform to unconfigure the S3 backend and
+# move the tfstate file to a local tfstate file
 terraform init -force-copy
 
+# Finally destroy the infrastructure
 terraform destroy -auto-approve
