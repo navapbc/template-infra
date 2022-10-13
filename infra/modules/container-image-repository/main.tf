304f56b89c4e9dc9438354ffd96fe10e7a452cc5
Loren Yu <loren@navapbc.com>
2022-10-13 11:32:11 -0700

    Add push and pull access policies

--- infra/modules/container-image-repository/main.tf
+++ infra/modules/container-image-repository/main.tf
@@ -22,6 +22,9 @@
 
 locals {
   image_repository_name = "${var.project_name}-${var.app_name}"
+
+  # Friendly path to put IAM policies created by this module
+  iam_path = "/${var.project_name}/${var.app_name}/container-image-repository/"
 }
 
 resource "aws_ecr_repository" "app" {
@@ -53,4 +56,45 @@ resource "aws_ecr_lifecycle_policy" "image_retention" {
     ]
 }
 EOF
+}
+
+data "aws_iam_policy_document" "push_access" {
+  statement {
+    sid = "PushAccess"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart"
+    ]
+    effect = "Allow"
+    resources = [ "${aws_ecr_repository.app.arn}" ]
+  }
+}
+
+data "aws_iam_policy_document" "pull_access" {
+  statement {
+    sid = "PullAccess"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+    ]
+    effect = "Allow"
+    resources = [ "${aws_ecr_repository.app.arn}" ]
+  }
+}
+
+resource "aws_iam_policy" "push_access" {
+  name = "push-access"
+  path = local.iam_path
+  description = "Allow push access to the ECR repository for ${var.app_name}"
+  policy = data.aws_iam_policy_document.push_access.json
+}
+
+resource "aws_iam_policy" "pull_access" {
+  name = "pull-access"
+  path = local.iam_path
+  description = "Allow pull access to the ECR repository for ${var.app_name}"
+  policy = data.aws_iam_policy_document.pull_access.json
 }
