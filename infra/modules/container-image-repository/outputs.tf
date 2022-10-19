db2228abb224f9aecddd51c320de3822e41a5448
Loren Yu <loren@navapbc.com>
2022-10-18 17:34:36 -0700

    Add repository url to repo output

--- infra/modules/container-image-repository/outputs.tf
+++ infra/modules/container-image-repository/outputs.tf
@@ -1,3 +1,7 @@
 output "image_repository_name" {
-  value = local.image_repository_name
+  value = aws_ecr_repository.app.name
+}
+
+output "image_repository_url" {
+  value = aws_ecr_repository.app.repository_url
 }
