85112d1abbebc7e37c9699542af6a90d1e1a9c68
Loren Yu <loren@navapbc.com>
2022-10-19 10:34:42 -0700

    Refactor test

--- template-only-test/account_setup_test.go
+++ template-only-test/account_setup_test.go
@@ -9,40 +9,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// An example of how to test the simple Terraform module in examples/terraform-basic-example using Terratest.
 func TestAccountSetup(t *testing.T) {
-	t.Parallel()
-
 	// Note: projectName can't be too long since S3 bucket names have a 63 character max length
 	projectName := "platform-test-account"
 	accountId := "368823044688"
-	githubActionsRole := fmt.Sprintf("arn:aws:iam::%s:role/%s-github-actions", accountId, projectName)
 
 	region := "us-east-1"
-	expectedTfStateBucket := "platform-template-infra-368823044688-us-east-1-tf-state"
-	expectedTfStateKey := "platform-template-infra/infra/account.tfstate"
 
-	defer shell.RunCommand(t, shell.Command{
-		Command:    "make",
-		Args:       []string{"-f", "template-only.mak", "destroy-account"},
-		WorkingDir: "../",
-	})
+	defer TeardownAccount(t)
+	SetUpAccount(t)
+	ValidateTerraformBackend(t, region, projectName)
+	ValidateGithubActionsAuth(t, accountId, projectName)
+}
 
-	shell.RunCommand(t, shell.Command{
-		Command:    "make",
-		Args:       []string{"-f", "template-only.mak", "set-up-account", fmt.Sprintf("PROJECT_NAME=%s", projectName)},
-		WorkingDir: "../",
-	})
-
+func ValidateTerraformBackend(t *testing.T, region string, projectName string) {
+	expectedTfStateBucket := fmt.Sprintf("%s-368823044688-%s-tf-state", projectName, region)
+	expectedTfStateKey := fmt.Sprintf("%s/infra/account.tfstate", projectName)
 	aws.AssertS3BucketExists(t, region, expectedTfStateBucket)
 	_, err := aws.GetS3ObjectContentsE(t, region, expectedTfStateBucket, expectedTfStateKey)
-	assert.NoError(t, err, "Failed to get tfstate object from tfstate bucket")
+	assert.NoError(t, err, fmt.Sprintf("Failed to get tfstate object from tfstate bucket %s", expectedTfStateBucket))
+}
 
+func ValidateGithubActionsAuth(t *testing.T, accountId string, projectName string) {
+	githubActionsRole := fmt.Sprintf("arn:aws:iam::%s:role/%s-github-actions", accountId, projectName)
 	// Check that GitHub Actions can authenticate with AWS
-	err = shell.RunCommandE(t, shell.Command{
+	err := shell.RunCommandE(t, shell.Command{
 		Command:    "make",
 		Args:       []string{"-f", "template-only.mak", "check-github-actions-auth", fmt.Sprintf("GITHUB_ACTIONS_ROLE=%s", githubActionsRole)},
 		WorkingDir: "../",
 	})
 	assert.NoError(t, err, "GitHub actions failed to authenticate")
+}
+
+func SetUpAccount(t *testing.T) {
+	shell.RunCommand(t, shell.Command{
+		Command:    "make",
+		Args:       []string{"-f", "template-only.mak", "set-up-account", fmt.Sprintf("PROJECT_NAME=%s", projectName)},
+		WorkingDir: "../",
+	})
+}
+
+func TeardownAccount(t *testing.T) {
+	shell.RunCommand(t, shell.Command{
+		Command:    "make",
+		Args:       []string{"-f", "template-only.mak", "destroy-account"},
+		WorkingDir: "../",
+	})
 }
