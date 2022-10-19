eda74b0279056a322bd1abdaaafb80d0530021a4
Loren Yu <loren@navapbc.com>
2022-10-18 17:30:32 -0700

    Add print statements

--- bin/set-up-app-build-repository.sh
+++ bin/set-up-app-build-repository.sh
@@ -17,9 +17,17 @@ echo "Setting up build-repository"
 
 cd infra/$APP_NAME/build-repository
 
+echo "----------------------------------------------"
+echo "Replace placeholder values in terraform.tfvars"
+echo "----------------------------------------------"
+
 # Replace placeholder value of GitHub actions role with actual value
 # Use '|' as sed command separator since role ARN can have '/' characters
 sed -i .bak "s|<GITHUB_ACTIONS_ROLE_ARN>|$GITHUB_ACTIONS_ROLE_ARN|g" terraform.tfvars
+
+echo "-------------------------------"
+echo "Deploy infrastructure resources"
+echo "-------------------------------"
 
 terraform init
 terraform apply -auto-approve
