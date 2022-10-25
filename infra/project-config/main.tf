0abf5ecbf679bfda15445a0e60e9c36384ff0344
Loren Yu <loren@navapbc.com>
2022-10-25 15:55:56 -0700

    Update instructions

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,6 +1,13 @@
 locals {
-  project_name        = "<PROJECT_NAME>"
-  owner               = "<OWNER>"
+  # Machine readable project name (lower case letters, dashes, and underscores)
+  project_name = "<PROJECT_NAME>"
+
+  # Project owner
+  owner = "<OWNER>"
+
+  # URL of project source code repository
   code_repository_url = "<REPO_URL>"
-  default_region      = "<DEFAULT_REGION>"
+
+  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1)
+  default_region = "<DEFAULT_REGION>"
 }
