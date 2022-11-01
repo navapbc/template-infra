1fd9638ac423c43dc1ed24d81507d833b1113c80
Loren Yu <loren@navapbc.com>
2022-11-01 10:28:38 -0700

    Rename cluster to app-service

--- infra/app/env-template/main.tf
+++ infra/app/env-template/main.tf
@@ -1,9 +1,4 @@
-# ECS cluster for running applications in an environment.
-#
-# Applications within this cluster are not actually tied
-# to a VPC/environment, but we separate them to improve
-# organization and simplify lookups.
-module "cluster" {
-  source = "../../modules/cluster"
+module "service" {
+  source = "../../modules/app-service"
   name   = var.environment_name
 }
