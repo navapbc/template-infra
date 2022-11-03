443878c1281ec5ace008b58a35a4638f22387b8b
Loren Yu <loren@navapbc.com>
2022-11-03 15:56:32 -0700

    Add missing required args to app-service

--- infra/app/env-template/main.tf
+++ infra/app/env-template/main.tf
@@ -1,3 +1,20 @@
+# TODO(https://github.com/navapbc/template-infra/issues/152) use non-default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# TODO(https://github.com/navapbc/template-infra/issues/152) use private subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "default-for-az"
+    values = [true]
+  }
+}
+
+data "aws_ecr_repository" "app" {
+  name = module.app_config.image_repository_name
+}
+
 locals {
   project_name = module.project_config.project_name
   app_name     = module.app_config.app_name
@@ -15,4 +32,6 @@ module "app_config" {
 module "service" {
   source       = "../../modules/app-service"
   service_name = local.service_name
+  vpc_id       = data.aws_vpc.default.id
+  image_url    = "${data.aws_ecr_repository.app.repository_url}:${var.image_tag}"
 }
--- infra/app/env-template/variables.tf
+++ infra/app/env-template/variables.tf
@@ -2,3 +2,8 @@ variable "environment_name" {
   type        = string
   description = "name of the application environment"
 }
+
+variable "image_tag" {
+  type        = string
+  description = "image tag to deploy to the environment"
+}
