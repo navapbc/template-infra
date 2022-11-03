26170c8c1db981690ab58fa525b0f7cec5cab7d0
Loren Yu <loren@navapbc.com>
2022-11-03 15:46:00 -0700

    Define service_name

--- infra/app/env-template/main.tf
+++ infra/app/env-template/main.tf
@@ -1,4 +1,18 @@
+locals {
+  project_name = module.project_config.project_name
+  app_name     = module.app_config.app_name
+  service_name = "${local.project_name}-${local.app_name}-${var.environment_name}"
+}
+
+module "project_config" {
+  source = "../../project-config"
+}
+
+module "app_config" {
+  source = "../app-config"
+}
+
 module "service" {
-  source = "../../modules/app-service"
-  name   = var.environment_name
+  source       = "../../modules/app-service"
+  service_name = local.service_name
 }
