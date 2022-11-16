32500bb92ebbe2c4d03576fadfdd65b8be1281cb
Loren Yu <loren@navapbc.com>
2022-11-16 08:56:22 -0800

    Add account id back and update log message

--- bin/deploy-release.sh
+++ bin/deploy-release.sh
@@ -5,14 +5,18 @@ APP_NAME=$1
 IMAGE_TAG=$2
 ENV_NAME=$3
 
+# Need to init module when running in CD since GitHub actions does a fresh checkout of repo
+terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME init
+ACCOUNT_ID=$(terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME output -raw account_id)
+
 echo "--------------------------"
 echo "Deploy release parameters"
 echo "--------------------------"
 echo "APP_NAME=$APP_NAME"
 echo "IMAGE_TAG=$IMAGE_TAG"
-echo "IMAGE_NAME=$ENV_NAME"
+echo "ENV_NAME=$ENV_NAME"
+echo "ACCOUNT_ID=$ACCOUNT_ID"
 echo
-echo "Deploy image to AWS"
-terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME init
+echo "Starting $APP_NAME deploy of $IMAGE_TAG to $ENV_NAME"
 terraform -chdir=infra/$APP_NAME/envs/$ENV_NAME apply -auto-approve -var="image_tag=$IMAGE_TAG"
-echo "Deploy success"
+echo "Completed $APP_NAME deploy of $IMAGE_TAG to $ENV_NAME"
