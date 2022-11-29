08e39cfb2491ea44a4d97edaf314612b25508b92
Danielle Swyhart <hootee77@hotmail.com>
2022-11-29 09:10:14 -0600

    corrected lint errors

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -12,37 +12,37 @@ module "project_config" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
-  
+
   # Basic Details
   name = local.vpc_name
   cidr = "${local.cidr_prefix}0.0/16"
 
   # Subnets availablity zones
   azs = ["${local.region}a", "${local.region}b", "${local.region}c", "${local.region}d", "${local.region}e", "${local.region}f"]
-  
+
   # Subnet - Private 
-  private_subnets      = ["${local.cidr_prefix}0.0/20","${local.cidr_prefix}16.0/20", "${local.cidr_prefix}32.0/20"]
+  private_subnets      = ["${local.cidr_prefix}0.0/20", "${local.cidr_prefix}16.0/20", "${local.cidr_prefix}32.0/20"]
   enable_nat_gateway   = true
   single_nat_gateway   = true # Disable if you want a nat gateway for each AZ. This will become expensive
   private_subnet_names = ["Private Subnet One", "Private Subnet Two", "Private Subnet Three"]
-  private_subnet_tags  = {
+  private_subnet_tags = {
     Name = "${local.project_name}-private-subnets"
   }
 
   # Subnet - Public 
   public_subnets      = ["${local.cidr_prefix}48.0/20", "${local.cidr_prefix}64.0/20", "${local.cidr_prefix}80.0/20"]
-  public_subnet_names = ["Public Subnet One", "Public Subnet Two", "Public Subnet Three"] 
-  public_subnet_tags  = {
+  public_subnet_names = ["Public Subnet One", "Public Subnet Two", "Public Subnet Three"]
+  public_subnet_tags = {
     Name = "${local.project_name}-public-subnets"
   }
-#   # Database
-#   create_database_subnet_group       = true
-#   create_database_subnet_route_table = true
-#   database_subnets                   = ["${local.cidr_prefix}96.0/20", "${local.cidr_prefix}112.0/20", "${local.cidr_prefix}128.0/20"]
-#   database_subnet_names    = ["DB Subnet One", "DB Subnet Two", "DB Subnet Three"]
-#   database_subnet_tags = {
-#     Name = "${local.project_name}-DB-subnets"
-#   }
+  #   # Database
+  #   create_database_subnet_group       = true
+  #   create_database_subnet_route_table = true
+  #   database_subnets                   = ["${local.cidr_prefix}96.0/20", "${local.cidr_prefix}112.0/20", "${local.cidr_prefix}128.0/20"]
+  #   database_subnet_names    = ["DB Subnet One", "DB Subnet Two", "DB Subnet Three"]
+  #   database_subnet_tags = {
+  #     Name = "${local.project_name}-DB-subnets"
+  #   }
   # VPC DNS
   enable_dns_support   = true
   enable_dns_hostnames = true
--- infra/modules/vpc/outputs.tf
+++ infra/modules/vpc/outputs.tf
@@ -4,11 +4,11 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  description = "List of IDs of private subnets"  
-  value = module.vpc.private_subnets
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  description = "List of IDs of public subnets"  
-  value = module.vpc.public_subnets
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
 }
