0d4853094b27a194b516e7e081d76ae3c40bb4d7
Danielle Swyhart <hootee77@hotmail.com>
2022-11-29 08:13:38 -0600

    remove vpc and subnet default data blocks, add in the vpc module and remap the variables

--- infra/app/env-template/main.tf
+++ infra/app/env-template/main.tf
@@ -1,16 +1,3 @@
-# TODO(https://github.com/navapbc/template-infra/issues/152) use non-default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# TODO(https://github.com/navapbc/template-infra/issues/152) use private subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "default-for-az"
-    values = [true]
-  }
-}
-
 locals {
   # The prefix key/value pair is used for Terraform Workspaces, which is useful for projects with multiple infrastructure developers.
   # By default, Terraform creates a workspace named “default.” If a non-default workspace is not created this prefix will equal “default”, 
@@ -18,6 +5,10 @@ locals {
   prefix       = terraform.workspace == "default" ? "" : "${terraform.workspace}-"
   app_name     = module.app_config.app_name
   service_name = "${local.prefix}${local.app_name}-${var.environment_name}"
+}
+
+module "aws_vpc" {
+  source = "../../modules/vpc"
 }
 
 module "project_config" {
@@ -33,6 +24,7 @@ module "service" {
   service_name          = local.service_name
   image_repository_name = module.app_config.image_repository_name
   image_tag             = var.image_tag
-  vpc_id                = data.aws_vpc.default.id
-  subnet_ids            = data.aws_subnets.default.ids
+  vpc_id                = module.aws_vpc.vpc_id
+  subnet_ids            = module.aws_vpc.private_subnets
+  subnet_ids_public     = module.aws_vpc.public_subnets
 }
