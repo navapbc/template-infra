95ecbcbb069baf8dc714c972847db30eef07f91a
Nava-JoshLong <joshlong@navapbc.com>
2023-02-08 07:09:52 -0800

    Revert timeout config

--- template-only-test/template_infra_test.go
+++ template-only-test/template_infra_test.go
@@ -177,7 +177,7 @@ func ValidateDevEnvironment(t *testing.T) {
 		TerraformDir: "../infra/app/envs/dev/",
 	})
 	serviceEndpoint := terraform.Output(t, terraformOptions, "service_endpoint")
-	http_helper.HttpGetWithRetryWithCustomValidation(t, serviceEndpoint, nil, 10, 3*time.Second, func(responseStatus int, responseBody string) bool {
+	http_helper.HttpGetWithRetryWithCustomValidation(t, serviceEndpoint, nil, 5, 1*time.Second, func(responseStatus int, responseBody string) bool {
 		return responseStatus == 200
 	})
 }
