b378dc5a66062f3d49bdcc06b26b7658867ba07d
Loren Yu <loren@navapbc.com>
2023-04-04 14:35:57 -0700

    Fix destroy app service script

--- template-only-bin/destroy-app-service.sh
+++ template-only-bin/destroy-app-service.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 BACKEND_CONFIG_FILE="dev.s3.tfbackend"
 TF_VARS_FILE="dev.tfvars"
 
-cd infra/app/build-repository
+cd infra/app/service
 
 terraform init -reconfigure -backend-config=$BACKEND_CONFIG_FILE
 
