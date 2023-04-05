3698f6583029b8db6f15718fdc401d62b9747ddb
Michael Sanderson <michaelsanderson@navapbc.com>
2023-04-05 11:37:35 -0500

    downgrade checkov version

--- infra/modules/terraform-backend-s3/main.tf
+++ infra/modules/terraform-backend-s3/main.tf
@@ -55,41 +55,6 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
-data "aws_iam_policy_document" "topic" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    actions   = ["SNS:Publish"]
-    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
-
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = [aws_s3_bucket.tf_state.arn]
-    }
-  }
-}
-
-resource "aws_sns_topic" "topic" {
-  name   = "s3-event-notification-topic"
-  policy = data.aws_iam_policy_document.topic.json
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  topic {
-    topic_arn     = aws_sns_topic.topic.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
-  }
-}
-
 resource "aws_s3_bucket_versioning" "tf_state" {
   bucket = aws_s3_bucket.tf_state.id
   versioning_configuration {
