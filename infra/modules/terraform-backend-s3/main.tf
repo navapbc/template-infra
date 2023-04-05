7fa0bbdb0e4f359be940ec640f603dcee48a2434
Michael Sanderson <michaelsanderson@navapbc.com>
2023-04-05 17:23:43 -0500

    Add ignore for lifecycle management on tf_state

--- infra/modules/terraform-backend-s3/main.tf
+++ infra/modules/terraform-backend-s3/main.tf
@@ -48,6 +48,7 @@ resource "aws_s3_bucket" "tf_state" {
 
   # checkov:skip=CKV_AWS_144:Cross region replication not required by default
   # checkov:skip=CKV2_AWS_62:S3 bucket does not need notifications enabled
+  # checkov:skip=CKV2_AWS_61:This s3 bucket holds state files that we do not need to delete
 
   # Prevent accidental destruction a developer executing terraform destory in the wrong directory. Contains terraform state files.
   lifecycle {
