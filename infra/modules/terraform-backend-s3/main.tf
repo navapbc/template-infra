9caed41a16b30353179b7fcd1b03f28c34b05eae
navams <88730205+navams@users.noreply.github.com>
2023-04-06 10:30:50 -0500

    #247-Ignore checkov lifecycle configuration and notification rules for tf_state s3 bucket (#246)
    ## Ticket
    
    Resolves https://github.com/navapbc/template-infra/issues/247
    
    ## Changes
    Ignore lifecycle configuration and enabled notification rules for
    tf_state s3 bucket
    
    ## Context for reviewers
    Compliance test should pass
    
    ## Testing
    Compliance check passes

--- infra/modules/terraform-backend-s3/main.tf
+++ infra/modules/terraform-backend-s3/main.tf
@@ -47,6 +47,8 @@ resource "aws_s3_bucket" "tf_state" {
   bucket = local.tf_state_bucket_name
 
   # checkov:skip=CKV_AWS_144:Cross region replication not required by default
+  # checkov:skip=CKV2_AWS_62:S3 bucket does not need notifications enabled
+  # checkov:skip=CKV2_AWS_61:No need to define S3 bucket lifecycle configuration to expire or transition tfstate files since they will always be needed and the file sizes are small anyways
 
   # Prevent accidental destruction a developer executing terraform destory in the wrong directory. Contains terraform state files.
   lifecycle {
@@ -128,6 +130,7 @@ resource "aws_s3_bucket" "tf_log" {
   bucket = local.tf_logs_bucket_name
 
   # checkov:skip=CKV_AWS_144:Cross region replication not required by default
+  # checkov:skip=CKV2_AWS_62:S3 bucket does not need notifications enabled
 }
 
 resource "aws_s3_bucket_versioning" "tf_log" {
