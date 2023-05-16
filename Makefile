navapbc/template-infra

commit f80923a5d87980c12913fb23312f8e360045c20f

Separate tfbackend configs into separate files (#242)
* Separate terraform backend configs into separate *.s3.tfbackend files
* Separate environment configs into [env].tfvar files
* Rename env-template to service to represent application layer (in
preparation for adding a db layer)
* Remove envs folders now that the service (previously env-template)
module can act directly as the root module
* Add make commands for setting up account, app build repository, and
app service
* Add make commands for terraform init/applying the account, app build
repository, and app service
* Remove redundant make commands in template-only.mak
* Update template tests
* Update instructions
* Update module architecture documentation
* Update update.sh to not revert main.tf files
* Add ADR documenting the discussion and benefits of this approach
* Update ci-infra

Future todos created
* Add error messages when insufficient parameters passed (will be
addressed in https://github.com/navapbc/template-infra/issues/263)

Makefile
@@ -6,6 +6,12 @@ PROJECT_NAME ?= $(notdir $(PWD))
 # on will be determined by the APP_NAME Makefile argument
 APP_NAME ?= app
 
+# Use `=` instead of `:=` so that we only execute `./bin/current-account-alias.sh` when needed
+# See https://www.gnu.org/software/make/manual/html_node/Flavors.html#Flavors
+CURRENT_ACCOUNT_ALIAS = `./bin/current-account-alias.sh`
+
+CURRENT_ACCOUNT_ID = $(./bin/current-account-id.sh)
+
 # Get the list of reusable terraform modules by getting out all the modules
 # in infra/modules and then stripping out the "infra/modules/" prefix
 MODULES := $(notdir $(wildcard infra/modules/*))
@@ -13,6 +19,21 @@ MODULES := $(notdir $(wildcard infra/modules/*))
 # Get the list of accounts and environments in a manner similar to MODULES above
 ACCOUNTS := $(notdir $(wildcard infra/accounts/*))
 ENVIRONMENTS := $(notdir $(wildcard infra/app/envs/*))
+
+# Check that given variables are set and all have non-empty values,
+# die with an error otherwise.
+#
+# Params:
+#   1. Variable name(s) to test.
+#   2. (optional) Error message to print.
+# Based off of https://stackoverflow.com/questions/10858261/how-to-abort-makefile-if-variable-not-set
+check_defined = \
+	$(strip $(foreach 1,$1, \
+        $(call __check_defined,$1,$(strip $(value 2)))))
+__check_defined = \
+	$(if $(value $1),, \
+		$(error Undefined $1$(if $2, ($2))$(if $(value @), \
+			required by target `$@')))
 
 
 .PHONY : \
@@ -30,6 +51,26 @@ ENVIRONMENTS := $(notdir $(wildcard infra/app/envs/*))
 	db-migrate \
 	db-migrate-down \
 	db-migrate-create
+
+infra-set-up-account:  # Set up the AWS account for the first time
+	@:$(call check_defined, ACCOUNT_NAME, human readable name for account e.g. "prod" or the AWS account alias)
+	./bin/set-up-current-account.sh $(ACCOUNT_NAME)
+
+infra-configure-app-build-repository:
+	./bin/configure-app-build-repository.sh $(APP_NAME)
+
+infra-configure-app-service:
+	./bin/configure-app-service.sh $(APP_NAME) $(ENVIRONMENT)
+
+infra-update-current-account:
+	./bin/terraform-init-and-apply.sh infra/accounts `./bin/current-account-config-name.sh`
+
+infra-update-app-build-repository:
+	./bin/terraform-init-and-apply.sh infra/$(APP_NAME)/build-repository shared
+
+infra-update-app-service:
+	./bin/terraform-init-and-apply.sh infra/$(APP_NAME)/service $(ENVIRONMENT)
+
 
 # Validate all infra root and child modules.
 infra-validate: \
@@ -99,11 +140,11 @@ release-publish:
 	./bin/publish-release.sh $(APP_NAME) $(IMAGE_NAME) $(IMAGE_TAG)
 
 release-deploy:
-# check the varaible against the list of enviroments and suggest one of the correct envs.
-ifneq ($(filter $(ENV_NAME),$(ENVIRONMENTS)),)
-	./bin/deploy-release.sh $(APP_NAME) $(IMAGE_TAG) $(ENV_NAME)
+# check the variable against the list of enviroments and suggest one of the correct envs.
+ifneq ($(filter $(ENVIRONMENT),$(ENVIRONMENTS)),)
+	./bin/deploy-release.sh $(APP_NAME) $(IMAGE_TAG) $(ENVIRONMENT)
 else
-	@echo "Please enter: make release-deploy ENV_NAME=<env_name>. The value for env_name must be one of these: $(ENVIRONMENTS)"
+	@echo "Please enter: make release-deploy ENVIRONMENT=<environment>. The value for environment must be one of these: $(ENVIRONMENTS)"
 	exit 1
 endif
 

commit 231922b37f96613bf1d2daa8824589e5e2fb625b

Fix CD (#271)
* Remove obsolete Makefile variables
* Replace old environment variable check

## Context
Fixes an issue introduced by #242 

A rename of a Makefile parameter from ENV_NAME to ENVIRONMENT caused cd
workflow to break as part of the refactor to seprate tfbackend configs
into separate files. See
https://github.com/navapbc/platform-test/actions/runs/4984413454

Moreover, the ENVIRONMENTS variable no longer works since the `envs`
folder has been deleted

.github/workflows/cd.yml
@@ -11,8 +11,8 @@ on:
   #     - 'infra/**'
   workflow_dispatch:
     inputs:
-      env_name:
-        description: "env_name input"
+      environment:
+        description: "environment input"
         required: true
         default: "dev"
         type: choice
@@ -22,10 +22,10 @@ on:
           - prod
 
 env:
-  ENV_NAME: ${{ inputs.env_name || 'dev' }}
+  ENVIRONMENT: ${{ inputs.environment || 'dev' }}
 
 # Need to repeat the expression since env.ENV_NAME is not accessible in this context
-concurrency: cd-${{ inputs.env_name || 'dev' }}
+concurrency: cd-${{ inputs.environment || 'dev' }}
 
 jobs:
   deploy:
@@ -52,4 +52,4 @@ jobs:
         run: make release-publish
 
       - name: Deploy release
-        run: make release-deploy ENV_NAME="$ENV_NAME"
+        run: make release-deploy ENVIRONMENT="$ENVIRONMENT"

Makefile
@@ -16,10 +16,6 @@ CURRENT_ACCOUNT_ID = $(./bin/current-account-id.sh)
 # in infra/modules and then stripping out the "infra/modules/" prefix
 MODULES := $(notdir $(wildcard infra/modules/*))
 
-# Get the list of accounts and environments in a manner similar to MODULES above
-ACCOUNTS := $(notdir $(wildcard infra/accounts/*))
-ENVIRONMENTS := $(notdir $(wildcard infra/app/envs/*))
-
 # Check that given variables are set and all have non-empty values,
 # die with an error otherwise.
 #
@@ -148,13 +144,8 @@ release-publish:
 	./bin/publish-release.sh $(APP_NAME) $(IMAGE_NAME) $(IMAGE_TAG)
 
 release-deploy:
-# check the variable against the list of enviroments and suggest one of the correct envs.
-ifneq ($(filter $(ENVIRONMENT),$(ENVIRONMENTS)),)
+	@:$(call check_defined, ENVIRONMENT, the name of the application environment e.g. "prod" or "dev")
 	./bin/deploy-release.sh $(APP_NAME) $(IMAGE_TAG) $(ENVIRONMENT)
-else
-	@echo "Please enter: make release-deploy ENVIRONMENT=<environment>. The value for environment must be one of these: $(ENVIRONMENTS)"
-	exit 1
-endif
 
 release-image-name: ## Prints the image name of the release image
 	@echo $(IMAGE_NAME)