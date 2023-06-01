navapbc/template-infra

commit ca9b0d57b6181bef63662db9aa9f6095d8120e5b

Add location for buckets outside of us-east-1 (#292)
Fix for a bug that was encountered while testing the template with `us-west-1`

bin/set-up-current-account.sh
@@ -49,7 +49,7 @@ echo "Bootstrapping the account by creating an S3 backend with minimal configura
 echo "------------------------------------------------------------------------------"
 echo 
 echo "Creating bucket: $TF_STATE_BUCKET_NAME"
-aws s3api create-bucket --bucket $TF_STATE_BUCKET_NAME --region $REGION > /dev/null
+aws s3api create-bucket --bucket $TF_STATE_BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION > /dev/null
 echo
 echo "----------------------------------"
 echo "Creating rest of account resources"

commit 3f7a73b0a4de691d674724614dc835ed5e1a3a05

Fix regression for us-east-1 (#296)
Fix regression introduced by #292

bin/set-up-current-account.sh
@@ -49,7 +49,14 @@ echo "Bootstrapping the account by creating an S3 backend with minimal configura
 echo "------------------------------------------------------------------------------"
 echo 
 echo "Creating bucket: $TF_STATE_BUCKET_NAME"
-aws s3api create-bucket --bucket $TF_STATE_BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION > /dev/null
+# For creating buckets outside of us-east-1, a LocationConstraint needs to be set
+# For creating buckets in us-east-1, LocationConstraint cannot be set
+# See https://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html
+CREATE_BUCKET_CONFIGURATION=""
+if [ $REGION != "us-east-1" ]; then
+  CREATE_BUCKET_CONFIGURATION="--create-bucket-configuration LocationConstraint=$REGION"
+fi
+aws s3api create-bucket --bucket $TF_STATE_BUCKET_NAME --region $REGION $CREATE_BUCKET_CONFIGURATION > /dev/null
 echo
 echo "----------------------------------"
 echo "Creating rest of account resources"