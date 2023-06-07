4f74910d0a1542d0a1b4c39b25b0dfffeda4d8b8
Loren Yu <loren@navapbc.com>
2023-06-07 08:05:28 -0700

    Use different style of quotes
    Co-authored-by: James Bursa <james.c.bursa@mass.gov>

--- infra/modules/database/role_manager/role_manager.py
+++ infra/modules/database/role_manager/role_manager.py
@@ -50,10 +50,10 @@ def connect() -> Connection:
 
 
 def get_roles(conn: Connection) -> list[str]:
-    return [row[0] for row in conn.run("SELECT rolname \
-                                       FROM pg_roles \
-                                       WHERE rolname NOT LIKE 'pg_%'\
-                                       AND rolname NOT LIKE 'rds%'")]
+    return [row[0] for row in conn.run("SELECT rolname "
+                                       "FROM pg_roles "
+                                       "WHERE rolname NOT LIKE 'pg_%' "
+                                       "AND rolname NOT LIKE 'rds%'")]
 
 
 def get_roles_with_groups(conn: Connection) -> list[tuple[str, str]]:
