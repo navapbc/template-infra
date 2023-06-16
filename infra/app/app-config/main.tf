52cd0fa8199bdba9ebc5467d4715a496346e3d4f
Loren Yu <loren@navapbc.com>
2023-06-16 12:56:54 -0700

    Default has_database back to false

--- infra/app/app-config/main.tf
+++ infra/app/app-config/main.tf
@@ -3,7 +3,7 @@ locals {
   environments          = ["dev", "staging", "prod"]
   project_name          = module.project_config.project_name
   image_repository_name = "${local.project_name}-${local.app_name}"
-  has_database          = true
+  has_database          = false
   environment_configs   = { for environment in local.environments : environment => module.env_config[environment] }
 }
 
