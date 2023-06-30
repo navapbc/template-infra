a369c1a8e5cf07a74fb1428e8b8b1ecbda1f036d
Iryna Shafinska <irynashafinska@navapbc.com>
2023-06-30 17:53:00 -0400

    feat: remove threshold configuration, resource names

--- infra/modules/monitoring/variables.tf
+++ infra/modules/monitoring/variables.tf
@@ -1,28 +1,9 @@
 variable "service_name" {
   type        = string
-  description = "List of services running within ECS cluster"
+  description = "Name of the service running within ECS cluster"
 }
 
 variable "load_balancer_arn_suffix" {
   type        = string
   description = "The ARN suffix for use with CloudWatch Metrics."
 }
-
-variable "high_http_target_5xx_error_count_threshold" {
-  type        = number
-  default     = null
-  description = "High http service 5XX error count threshold"
-}
-
-variable "high_http_elb_5xx_error_count_threshold" {
-  type        = number
-  default     = null
-  description = "High http ELB 5XX error count threshold"
-}
-
-variable "high_target_response_time_threshold" {
-  type        = number
-  default     = null
-  description = "High service latency threshold"
-}
-
