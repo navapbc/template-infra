35061bc84acc6a0802d4f3cf0dd8bfc9e34fab85
Iryna Shafinska <IrynaShafinska@gmail.com>
2023-06-30 18:57:46 -0400

    feat: remove threshold configuration, resource names (#344)
    ## Ticket
    https://github.com/navapbc/template-infra/issues/328
    
    ## Changes
    This PR is to address final comments from
    https://github.com/navapbc/template-infra/pull/336
    
    ## Context for reviewers
    Removed configurable thresholds for alerts and some of the resources are
    renamed
    
    ## Testing
    Changes applied to test account
    automated tests
    
    ---------
    
    Co-authored-by: Iryna Shafinska <irynashafinska@navapbc.com>

--- infra/modules/monitoring/main.tf
+++ infra/modules/monitoring/main.tf
@@ -1,23 +1,22 @@
 # Create SNS topic for all email and external incident management tools notifications
 
 resource "aws_sns_topic" "this" {
-  name = "${var.service_name}-monitoring-notifications"
+  name = "${var.service_name}-monitoring"
 
   # checkov:skip=CKV_AWS_26:SNS encryption for alerts is unnecessary 
 }
 
 # Create CloudWatch alarms for the service
 
-resource "aws_cloudwatch_metric_alarm" "high_http_target_5xx_error_count_threshold" {
-  count                     = var.high_http_target_5xx_error_count_threshold != null ? 1 : 0
-  alarm_name                = "${var.service_name}-high-http-target-5xx-error-count-threshold"
+resource "aws_cloudwatch_metric_alarm" "high_app_http_5xx_count" {
+  alarm_name                = "${var.service_name}-high-app-5xx-count"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 5
   metric_name               = "HTTPCode_Target_5XX_Count"
   namespace                 = "AWS/ApplicationELB"
   period                    = 60
   statistic                 = "Sum"
-  threshold                 = var.high_http_target_5xx_error_count_threshold
+  threshold                 = 1
   alarm_description         = "High HTTP service 5XX error count"
   alarm_actions             = [aws_sns_topic.this.arn]
   ok_actions                = [aws_sns_topic.this.arn]
@@ -28,16 +27,15 @@ resource "aws_cloudwatch_metric_alarm" "high_http_target_5xx_error_count_thresho
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "high_http_elb_5xx_error_count_threshold" {
-  count                     = var.high_http_elb_5xx_error_count_threshold != null ? 1 : 0
-  alarm_name                = "${var.service_name}-high-http-elb-5xx-error-count-threshold"
+resource "aws_cloudwatch_metric_alarm" "high_load_balancer_http_5xx_count" {
+  alarm_name                = "${var.service_name}-high-load-balancer-5xx-count"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 5
   metric_name               = "HTTPCode_ELB_5XX_Count"
   namespace                 = "AWS/ApplicationELB"
   period                    = 60
   statistic                 = "Sum"
-  threshold                 = var.high_http_elb_5xx_error_count_threshold
+  threshold                 = 1
   alarm_description         = "High HTTP ELB 5XX error count"
   alarm_actions             = [aws_sns_topic.this.arn]
   ok_actions                = [aws_sns_topic.this.arn]
@@ -48,16 +46,15 @@ resource "aws_cloudwatch_metric_alarm" "high_http_elb_5xx_error_count_threshold"
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "high_target_response_time_threshold" {
-  count                     = var.high_target_response_time_threshold != null ? 1 : 0
-  alarm_name                = "${var.service_name}-high_target_response_time_threshold"
+resource "aws_cloudwatch_metric_alarm" "high_app_response_time" {
+  alarm_name                = "${var.service_name}-high-app-response-time"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 5
   metric_name               = "TargetResponseTime"
   namespace                 = "AWS/ApplicationELB"
   period                    = 60
   statistic                 = "Average"
-  threshold                 = var.high_target_response_time_threshold
+  threshold                 = 0.2
   alarm_description         = "High target latency alert"
   alarm_actions             = [aws_sns_topic.this.arn]
   ok_actions                = [aws_sns_topic.this.arn]
