04a83f1c157df713f61464051391bb1f27665327
Iryna Shafinska <irynashafinska@navapbc.com>
2023-07-20 10:07:30 -0400

    fix: revert project config defaults

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,16 +1,16 @@
 locals {
   # Machine readable project name (lower case letters, dashes, and underscores)
   # This will be used in names of AWS resources
-  project_name = "test"
+  project_name = "<PROJECT_NAME>"
 
   # Project owner
-  owner = "shafinska"
+  owner = "<OWNER>"
 
   # URL of project source code repository
-  code_repository_url = "git@github.com:gingeririna/infra.git"
+  code_repository_url = "<REPO_URL>"
 
   # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1)
-  default_region = "us-east-1"
+  default_region = "<DEFAULT_REGION>"
 
   github_actions_role_name = "${local.project_name}-github-actions"
 }
