d027ff6d575b064395e175c771435ffa797de053
Iryna Shafinska <irynashafinska@navapbc.com>
2023-07-20 11:28:05 -0400

    chore: remmove unused script

--- bin/get_monitoring_secret_name.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Print the name for SSM attribute that contains monitoring secret
-set -euo pipefail
-echo -n "$(cat infra/app/app-config/env-config/outputs.tf | grep integration_url_param_name | awk '{print $3}' | sed 's/var\.//g' | sed 's/\"//g' | sed 's/app_name/APP_NAME/' | sed 's/environment/ENVIRONMENT/' | sed 's/{//g' | sed 's/}//g')"
