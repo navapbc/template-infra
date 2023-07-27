e45b473760bb5acc07d98ab8812056813128ed49
FJOR <charlesfraley@navapbc.com>
2023-07-27 16:45:22 -0500

    Saving partway done before pivoting back to test repo

--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -133,13 +133,27 @@ resource "aws_lb_target_group" "app_tg" {
   }
 }
 
-#---------------
-## Load balancer logs
-#---------------
+#--------------------------
+## Load balancer log infra
+#--------------------------
 resource "aws_s3_bucket" "load_balancer_logs" {
   bucket_prefix = "${var.service_name}-access-logs"
   force_destroy = false
   # checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
+  # checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
+}
+
+resource "s3_bucket_public_access_block" "load_balancer_logs" {
+  bucket = aws_s3_bucket.load_balancer_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_replication_configuration" "load_balancer_logs" {
+  
 }
 
 # resource "aws_s3_bucket_lifecycle_configuration" "load_balancer_logs" {
@@ -166,11 +180,11 @@ resource "aws_s3_bucket_versioning" "load_balancer_logs" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "load_balancer" {
   bucket = aws_s3_bucket.load_balancer_logs.id
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.tf_backend.arn
       sse_algorithm     = "aws:kms"
     }
     bucket_key_enabled = true
@@ -179,7 +193,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
 
 resource "aws_s3_bucket_policy" "load_balancer_logs_put_access" {
   bucket = aws_s3_bucket.load_balancer_logs.id
-  policy = data.aws_iam_policy_document.log_access_bucket_pol_doc.json
+  policy = data.aws_iam_policy_document.load_balancer_logs_put_access.json
 }
 
 data "aws_iam_policy_document" "load_balancer_logs_put_access" {
