3b7651d460ce5d28f4af63596ccd6954c74c8b8e
Alsia Plybeah <alsia@navapbc.com>
2023-08-01 11:12:58 -0400

    update scripts for test workflow

--- template-only-bin/update-template.sh
+++ template-only-bin/update-template.sh
@@ -14,15 +14,28 @@ $SCRIPT_DIR/install-template.sh
 # Also restore project files that had lines that were commented out in the template, such as Makefile
 # and cd.yml workflow
 # Updates in any of these files need to be manually applied to the projects
-echo "Restore modified project files"
-git checkout HEAD -- \
-  .dockleconfig \
-  .github/workflows/build-and-publish.yml \
-  .github/workflows/cd.yml \
-  .github/workflows/ci-infra.yml \
-  .github/workflows/database-migrations.yml \
-  .grype.yml \
-  .hadolint.yaml \
-  .trivyignore \
-  infra/project-config/main.tf \
-  infra/app/app-config/main.tf
+# echo "Restore modified project files"
+# git checkout HEAD -- \
+#   .dockleconfig \
+#   .github/workflows/build-and-publish.yml \
+#   .github/workflows/cd.yml \
+#   .github/workflows/ci-infra.yml \
+#   .github/workflows/database-migrations.yml \
+#   .grype.yml \
+#   .hadolint.yaml \
+#   .trivyignore \
+#   infra/project-config/main.tf \
+#   infra/app/app-config/main.tf
+
+
+cd template-infra
+# This is the HEAD of the main branch before the commit is merged
+CUR_VERSION=$(cat ./temp-track-template-version)
+
+# This should be the commit going in 
+NEW_VERSION=$(git rev-parse HEAD)
+git diff $CUR_VERSION -- filter > template.patch # filter certain files.
+# cd -
+git apply template.patch
+echo $NEW_VERSION # check new branch HEAD
+rm template.patch
