a5e6a66fadba307a91befaa9dd82383fbb6b9ff2
FJOR <charlesfraley@navapbc.com>
2023-08-08 17:43:09 -0500

    Moved s3 force destroy to appropriate script

--- template-only-bin/destroy-app-service.sh
+++ template-only-bin/destroy-app-service.sh
@@ -8,6 +8,10 @@ TF_VARS_FILE="dev.tfvars"
 
 cd infra/app/service
 
+sed -i.bak 's/resource "aws_s3_bucket" "load_balancer_logs" {/&\n  force_destroy = true/' ../modules/service/main.tf
+
 terraform init -reconfigure -backend-config=$BACKEND_CONFIG_FILE
+
+terraform apply -auto-approve -var-file=$TF_VARS_FILE
 
 terraform destroy -auto-approve -var-file=$TF_VARS_FILE
