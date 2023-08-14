f515e00e1ebc96872aadcbb48eb0261dffad289c
Alsia Plybeah <alsia@navapbc.com>
2023-08-14 17:10:56 -0400

    cleanup for PR

--- template-only-bin/update-template.sh
+++ template-only-bin/update-template.sh
@@ -9,24 +9,6 @@ SCRIPT_DIR=$(dirname $0)
 # echo "Install template"
 $SCRIPT_DIR/install-template.sh
 
-# Restore project files with project-specific configuration that was defined as part of project setup.
-# This includes the terraform backend configuration blocks and the project-config module
-# Also restore project files that had lines that were commented out in the template, such as Makefile
-# and cd.yml workflow
-# Updates in any of these files need to be manually applied to the projects
-# echo "Restore modified project files"
-# git checkout HEAD -- \
-  # .dockleconfig \
-  # .github/workflows/build-and-publish.yml \
-  # .github/workflows/cd.yml \
-  # .github/workflows/ci-infra.yml \
-  # .github/workflows/database-migrations.yml \
-  # .grype.yml \
-  # .hadolint.yaml \
-  # .trivyignore \
-  # infra/project-config/main.tf \
-  # infra/app/app-config/main.tf
-
 
 # This is the last commit from template-infra that the project had access to.
 PROJECT_VERSION=$(cat bin/template-version.txt)
@@ -46,8 +28,8 @@ git diff $PROJECT_VERSION $TEMPLATE_VERSION -- ':(exclude).github/workflows/temp
   :(exclude)infra/app/app-config/main.tf' -p > bin/template.patch 
 
 echo "Applying patch"
-git apply --ignore-whitespace --allow-empty template.patch
+git apply --ignore-whitespace --allow-empty bin/template.patch
 
 echo $TEMPLATE_VERSION > bin/template-version.txt
 # remove patch file
-rm template.patch
+rm bin/template.patch
