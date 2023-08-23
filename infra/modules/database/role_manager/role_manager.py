98ebe4f0e062558c73c40bf4e93dbbee3a87adb8
Loren Yu <loren@navapbc.com>
2023-08-22 17:30:23 -0700

    Grant connect privilege to users

--- infra/modules/database/role_manager/role_manager.py
+++ infra/modules/database/role_manager/role_manager.py
@@ -97,21 +97,22 @@ def configure_database(conn: Connection) -> None:
     app_username = os.environ.get("APP_USER")
     migrator_username = os.environ.get("MIGRATOR_USER")
     schema_name = os.environ.get("DB_SCHEMA")
+    database_name = os.environ.get("DB_NAME")
 
-    conn.run("REVOKE CREATE ON SCHEMA public FROM PUBLIC;")
-    conn.run("REVOKE ALL ON DATABASE app FROM PUBLIC;")
+    conn.run("REVOKE CREATE ON SCHEMA public FROM PUBLIC")
+    conn.run(f"REVOKE ALL ON DATABASE {identifier(database_name)} FROM PUBLIC")
 
-    configure_roles(conn, [migrator_username, app_username])
+    configure_roles(conn, [migrator_username, app_username], database_name)
     configure_schema(conn, schema_name, migrator_username, app_username)
 
 
-def configure_roles(conn: Connection, roles: list[str]) -> None:
+def configure_roles(conn: Connection, roles: list[str], database_name: str) -> None:
     logger.info("Configuring roles")
     for role in roles:
-        configure_role(conn, role)
+        configure_role(conn, role, database_name)
 
 
-def configure_role(conn: Connection, username: str) -> None:
+def configure_role(conn: Connection, username: str, database_name: str) -> None:
     logger.info("Configuring role: username=%s", username)
     role = "rds_iam"
     conn.run(
@@ -126,6 +127,7 @@ def configure_role(conn: Connection, username: str) -> None:
         """
     )
     conn.run(f"GRANT {identifier(role)} TO {identifier(username)}")
+    conn.run(f"GRANT CONNECT ON DATABASE {identifier(database_name)} TO {identifier(username)}")
 
 
 def configure_schema(conn: Connection, schema_name: str, migrator_username: str, app_username: str) -> None:
