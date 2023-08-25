22dfd3728703faafe798980c2606cc13b7b57f5c
Loren Yu <loren@navapbc.com>
2023-08-25 09:09:54 -0700

    Store template version on install

--- template-only-bin/download-and-install-template.sh
+++ template-only-bin/download-and-install-template.sh
@@ -7,5 +7,10 @@ git clone --single-branch --branch main --depth 1 git@github.com:navapbc/templat
 echo "Install template"
 ./template-infra/template-only-bin/install-template.sh
 
+# Store template version in a file
+cd template-infra
+git rev-parse HEAD > ../.template-version
+cd -
+
 echo "Clean up template-infra folder"
 rm -fr template-infra
