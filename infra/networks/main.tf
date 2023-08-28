navapbc/template-infra

commit c9529e55724e9e4abc8561f300b81072ba486abc

Migrate VPC endpoints to separate networks module (step 2/2) (#406)
## Changes

* Replace VPC endpoints in database module with data sources referencing
VPC endpoints created in network module

## Context for reviewers

This is the second part of migrating VPC endpoints to the network module
from the database module

## Rollout

As part of rolling out
https://github.com/navapbc/template-infra/pull/405 we imported VPC
endpoints to a separate networks module, so as part of rolling out this
module, we need to [terraform
rm](https://developer.hashicorp.com/terraform/cli/commands/state/rm) the
VPC endpoints from the database module before applying on platform-test
and platform-test-flask:

```bash
terraform -chdir=infra/app/database state rm module.database.aws_vpc_endpoint.kms
terraform -chdir=infra/app/database state rm module.database.aws_vpc_endpoint.ssm
make infra-update-app-database APP_NAME=app ENVIRONMENT=dev
```

infra/networks/main.tf
@@ -76,6 +76,8 @@ data "aws_subnets" "default" {
 # See https://docs.aws.amazon.com/vpc/latest/privatelink/create-interface-endpoint.html#create-interface-endpoint
 
 resource "aws_security_group" "aws_services" {
+  count = length(local.aws_service_integrations) > 0 ? 1 : 0
+
   name_prefix = module.project_config.aws_services_security_group_name_prefix
   description = "VPC endpoints to access AWS services from the VPCs private subnets"
   vpc_id      = data.aws_vpc.default.id

infra/app/database/main.tf
@@ -58,6 +58,18 @@ module "app_config" {
   source = "../app-config"
 }
 
+data "aws_security_groups" "aws_services" {
+  filter {
+    name   = "group-name"
+    values = ["${module.project_config.aws_services_security_group_name_prefix}*"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 module "database" {
   source = "../../modules/database"
 
@@ -65,10 +77,14 @@ module "database" {
   access_policy_name          = "${local.prefix}${local.database_config.access_policy_name}"
   app_access_policy_name      = "${local.prefix}${local.database_config.app_access_policy_name}"
   migrator_access_policy_name = "${local.prefix}${local.database_config.migrator_access_policy_name}"
-  app_username                = "${local.prefix}${local.database_config.app_username}"
-  migrator_username           = "${local.prefix}${local.database_config.migrator_username}"
-  schema_name                 = "${local.prefix}${local.database_config.schema_name}"
 
-  vpc_id             = data.aws_vpc.default.id
-  private_subnet_ids = data.aws_subnets.default.ids
+  # The following are not AWS infra resources and therefore do not need to be
+  # isolated via the terraform workspace prefix
+  app_username      = local.database_config.app_username
+  migrator_username = local.database_config.migrator_username
+  schema_name       = local.database_config.schema_name
+
+  vpc_id                         = data.aws_vpc.default.id
+  private_subnet_ids             = data.aws_subnets.default.ids
+  aws_services_security_group_id = data.aws_security_groups.aws_services.ids[0]
 }

infra/modules/database/networking.tf
@@ -33,42 +33,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_ingress_from_role_manager" {
   referenced_security_group_id = aws_security_group.role_manager.id
 }
 
-# VPC Endpoints for accessing AWS Services
-# ----------------------------------------
-#
-# Since the role manager Lambda function is in the VPC (which is needed to be
-# able to access the database) we need to allow the Lambda function to access
-# AWS Systems Manager Parameter Store (to fetch the database password) and
-# KMS (to decrypt SecureString parameters from Parameter Store). We can do
-# this by either allowing internet access to the Lambda, or by using a VPC
-# endpoint. The latter is more secure.
-# See https://repost.aws/knowledge-center/lambda-vpc-parameter-store
-# See https://docs.aws.amazon.com/vpc/latest/privatelink/create-interface-endpoint.html#create-interface-endpoint
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
-  vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  subnet_ids          = var.private_subnet_ids
-  private_dns_enabled = true
-}
-
-resource "aws_vpc_endpoint" "kms" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.kms"
-  vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.vpc_endpoints.id]
-  subnet_ids          = var.private_subnet_ids
-  private_dns_enabled = true
-}
-
-resource "aws_security_group" "vpc_endpoints" {
-  name_prefix = "${var.name}-vpc-endpoints"
-  description = "VPC endpoints to access SSM and KMS"
-  vpc_id      = var.vpc_id
-}
-
 resource "aws_vpc_security_group_egress_rule" "role_manager_egress_to_vpc_endpoints" {
   security_group_id = aws_security_group.role_manager.id
   description       = "Allow outbound requests from role manager to VPC endpoints"
@@ -76,11 +40,11 @@ resource "aws_vpc_security_group_egress_rule" "role_manager_egress_to_vpc_endpoi
   from_port                    = 443
   to_port                      = 443
   ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.vpc_endpoints.id
+  referenced_security_group_id = var.aws_services_security_group_id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "vpc_endpoints_ingress_from_role_manager" {
-  security_group_id = aws_security_group.vpc_endpoints.id
+  security_group_id = var.aws_services_security_group_id
   description       = "Allow inbound requests to VPC endpoints from role manager"
 
   from_port                    = 443

infra/modules/database/variables.tf
@@ -60,3 +60,8 @@ variable "private_subnet_ids" {
   type        = list(any)
   description = "list of private subnet IDs to put the role provisioner and role checker lambda functions in"
 }
+
+variable "aws_services_security_group_id" {
+  type        = string
+  description = "Security group ID for VPC endpoints that access AWS Services"
+}

commit e84baf5666514ac3a8fe7d1e8e08d8629faf896a

Fix terraform infra variable access (#408)
Fix terraform variable access bug introduced in #406:

infra/networks/main.tf
@@ -89,7 +89,7 @@ resource "aws_vpc_endpoint" "aws_service" {
   vpc_id              = data.aws_vpc.default.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.${each.key}"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.aws_services.id]
+  security_group_ids  = [aws_security_group.aws_services[0].id]
   subnet_ids          = data.aws_subnets.default.ids
   private_dns_enabled = true
 }