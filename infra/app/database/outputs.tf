248693b35b2ea0dcc0d0de64c1ea97a6fa8c0f54
Loren Yu <loren@navapbc.com>
2023-09-01 09:56:53 -0700

    Remove unneeded changes

--- infra/app/database/outputs.tf
+++ infra/app/database/outputs.tf
@@ -1,7 +1,3 @@
-output "cluster_id" {
-  value = module.database.cluster_id
-}
-
 output "role_manager_function_name" {
   value = module.database.role_manager_function_name
 }
--- infra/modules/database/outputs.tf
+++ infra/modules/database/outputs.tf
@@ -1,7 +1,3 @@
-output "cluster_id" {
-  value = aws_rds_cluster.db.id
-}
-
 output "role_manager_function_name" {
   value = aws_lambda_function.role_manager.function_name
 }
