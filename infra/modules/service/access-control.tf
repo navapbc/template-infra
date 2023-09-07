b461a69619c53f5be4fd4d7c02b5f6306ed9fe75
Loren Yu <loren@navapbc.com>
2023-09-07 15:48:09 -0700

    Remove conflicting empty managed policy block

--- infra/modules/service/access-control.tf
+++ infra/modules/service/access-control.tf
@@ -17,10 +17,6 @@ resource "aws_iam_role" "migrator_task" {
 
   name               = "${var.service_name}-migrator"
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume_role_policy.json
-
-  managed_policy_arns = [
-
-  ]
 }
 
 data "aws_iam_policy_document" "ecs_tasks_assume_role_policy" {
