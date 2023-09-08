f0f98963d6a3cb417a2ce3c4eb5fc3b56da067b7
Loren Yu <loren@navapbc.com>
2023-09-07 22:55:14 -0700

    Use make command in run db migrations

--- bin/run-database-migrations.sh
+++ bin/run-database-migrations.sh
@@ -46,7 +46,7 @@ echo "::group::Step 1. Update task definition without updating service"
 
 MODULE_DIR="infra/$APP_NAME/service"
 CONFIG_NAME="$ENVIRONMENT"
-TF_CLI_ARGS_apply="-input=false -auto-approve -target=module.service.aws_ecs_task_definition.app -var=image_tag=$IMAGE_TAG" ./bin/terraform-init-and-apply.sh "$MODULE_DIR" "$CONFIG_NAME"
+TF_CLI_ARGS_apply="-input=false -auto-approve -target=module.service.aws_ecs_task_definition.app -var=image_tag=$IMAGE_TAG" make infra-update-app-service APP_NAME="$APP_NAME" ENVIRONMENT="$ENVIRONMENT"
 
 echo "::endgroup::"
 echo
