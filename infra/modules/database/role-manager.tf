15d4e9b08741249e8c07893b01678108c66ff95a
FJOR <charlesfraley@navapbc.com>
2023-09-18 12:56:28 -0500

    Change triggers_replace value to timestamp()
    Resolves #446
    Causes pip install to occur on every apply

--- infra/modules/database/role-manager.tf
+++ infra/modules/database/role-manager.tf
@@ -49,10 +49,11 @@ resource "aws_lambda_function" "role_manager" {
 }
 
 # Installs python packages needed by the role manager lambda function before
-# creating the zip archive. Reinstalls whenever requirements.txt changes
+# creating the zip archive. 
+# Runs pip install on every apply.
+# Timestamp is used to always trigger replacement.
 resource "terraform_data" "role_manager_python_vendor_packages" {
-  triggers_replace = file("${path.module}/role_manager/requirements.txt")
-
+  triggers_replace = timestamp()
   provisioner "local-exec" {
     command = "pip3 install -r ${path.module}/role_manager/requirements.txt -t ${path.module}/role_manager/vendor"
   }
