4e9dd502b6dedc716c97540a9ea87b97f108ff0e
Rocket ! <rocketnova@users.noreply.github.com>
2023-11-13 13:20:21 -0800

    Add missing prefix needed for using workspaces

--- infra/app/service/main.tf
+++ infra/app/service/main.tf
@@ -64,17 +64,17 @@ module "app_config" {
 
 data "aws_rds_cluster" "db_cluster" {
   count              = module.app_config.has_database ? 1 : 0
-  cluster_identifier = local.database_config.cluster_name
+  cluster_identifier = "${local.prefix}${local.database_config.cluster_name}"
 }
 
 data "aws_iam_policy" "app_db_access_policy" {
   count = module.app_config.has_database ? 1 : 0
-  name  = local.database_config.app_access_policy_name
+  name  = "${local.prefix}${local.database_config.app_access_policy_name}"
 }
 
 data "aws_iam_policy" "migrator_db_access_policy" {
   count = module.app_config.has_database ? 1 : 0
-  name  = local.database_config.migrator_access_policy_name
+  name  = "${local.prefix}${local.database_config.migrator_access_policy_name}"
 }
 
 # Retrieve url for external incident management tool (e.g. Pagerduty, Splunk-On-Call)
