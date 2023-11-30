16b973e5190bd1d6fb8ad8e365f946191403b9f1
Loren Yu <loren@navapbc.com>
2023-11-29 17:29:27 -0800

    Ignore checkov

--- infra/modules/feature-flagging/logs.tf
+++ infra/modules/feature-flagging/logs.tf
@@ -1,6 +1,8 @@
 resource "aws_cloudwatch_log_group" "logs" {
   name = local.evidently_project_name
 
+  # checkov:skip=CKV_AWS_158:Feature flag evaluation logs are not sensitive
+
   # Conservatively retain logs for 5 years.
   # Looser requirements may allow shorter retention periods
   retention_in_days = 1827
