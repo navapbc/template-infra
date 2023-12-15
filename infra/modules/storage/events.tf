96203066624591a2f279a721b09ed12edfdea665
Loren Yu <loren@navapbc.com>
2023-12-15 08:26:18 -0800

    Fix typo in comment

--- infra/modules/storage/events.tf
+++ infra/modules/storage/events.tf
@@ -1,5 +1,5 @@
 # Tell S3 to publish events to EventBridge. Subscribers can then subscribe
-# to these events to in an event-based architecture.
+# to these events in an event-based architecture.
 # See https://docs.aws.amazon.com/AmazonS3/latest/userguide/ev-mapping-troubleshooting.html
 resource "aws_s3_bucket_notification" "storage" {
   bucket      = aws_s3_bucket.storage.id
