a4e4928a5c624a47d6b3cc7c19baaf270904f716
Loren Yu <loren@navapbc.com>
2023-12-18 14:31:56 -0800

    Remove unused config value

--- infra/app/app-config/env-config/outputs.tf
+++ infra/app/app-config/env-config/outputs.tf
@@ -2,7 +2,6 @@ output "database_config" {
   value = var.has_database ? {
     region                      = var.default_region
     cluster_name                = "${var.app_name}-${var.environment}"
-    access_policy_name          = "${var.app_name}-${var.environment}-db-access"
     app_username                = "app"
     migrator_username           = "migrator"
     schema_name                 = var.app_name
