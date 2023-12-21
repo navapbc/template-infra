449d3d03273047919f879211bdda7d5b0b4a641e
Loren Yu <loren@navapbc.com>
2023-12-21 11:59:24 -0800

    Move file_upload_jobs to separate file

--- infra/app/app-config/env-config/main.tf
+++ infra/app/app-config/env-config/main.tf
@@ -5,16 +5,4 @@ locals {
   prefix = terraform.workspace == "default" ? "" : "${terraform.workspace}-"
 
   bucket_name = "${local.prefix}${var.project_name}-${var.app_name}-${var.environment}"
-
-  # Configuration for default jobs to run in every environment.
-  # See description of `file_upload_jobs` variable in the service module (infra/modules/service/variables.tf)
-  # for the structure of this configuration object.
-  # One difference is that `source_bucket` is optional here. If `source_bucket` is not
-  # specified, then the source bucket will be set to the storage bucket's name
-  file_upload_jobs = {
-    etl = {
-      path_prefix  = "etl/input",
-      task_command = ["python", "-m", "flask", "--app", "app.py", "etl", "<object_key>"]
-    }
-  }
 }
