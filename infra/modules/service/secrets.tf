a6af215719af3350539d795c4283258d60025e69
Loren Yu <loren@navapbc.com>
2024-02-22 18:16:04 -0800

    Simplify secret definitions

--- infra/modules/service/secrets.tf
+++ infra/modules/service/secrets.tf
@@ -1,14 +1,9 @@
-data "aws_ssm_parameter" "secret" {
-  for_each = { for secret in var.secrets : secret.name => secret }
-  name     = each.value.ssm_param_name
-}
-
 locals {
   secrets = [
     for secret in var.secrets :
     {
       name      = secret.name,
-      valueFrom = data.aws_ssm_parameter.secret[secret.name].arn
+      valueFrom = secret.ssm_param_name
     }
   ]
 }
