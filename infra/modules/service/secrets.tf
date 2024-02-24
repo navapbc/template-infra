7640684314bc3ee919b797ef2f16e52c485e8c50
Loren Yu <loren@navapbc.com>
2024-02-23 19:10:25 -0800

    Use trimprefix

--- infra/modules/service/secrets.tf
+++ infra/modules/service/secrets.tf
@@ -9,6 +9,6 @@ locals {
 
   secret_arn_patterns = [
     for secret in var.secrets :
-    "arn:aws:ssm:*:*:parameter/${replace(secret.ssm_param_name, "/^//", "")}"
+    "arn:aws:ssm:*:*:parameter/${trimprefix(secret.ssm_param_name, "/")}"
   ]
 }
