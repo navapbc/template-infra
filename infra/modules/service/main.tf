03cb5ae651080a6639ec950508edaa3168d11e99
Rocket <rocketnova@users.noreply.github.com>
2024-05-01 15:19:29 -0700

    Incorporate feedback from @tevert: more legible

--- infra/modules/service/main.tf
+++ infra/modules/service/main.tf
@@ -80,7 +80,7 @@ resource "aws_ecs_task_definition" "app" {
       cpu                    = var.cpu,
       networkMode            = "awsvpc",
       essential              = true,
-      readonlyRootFilesystem = var.enable_service_execution ? !var.enable_service_execution : true,
+      readonlyRootFilesystem = !var.enable_service_execution,
 
       # Need to define all parameters in the healthCheck block even if we want
       # to use AWS's defaults, otherwise the terraform plan will show a diff
