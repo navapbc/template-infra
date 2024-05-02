a3496fb990ca1b3976ba78fdd5dc8fee39b2dc1b
Rocket <rocketnova@users.noreply.github.com>
2024-05-02 12:33:46 -0700

    Add script comments

--- template-only-bin/download-and-install-app.sh
+++ template-only-bin/download-and-install-app.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
-# This script renames the application template in your project.
+# This script downloads and installs infrastructure for an application.
 # Run this script in your project's root directory.
 #
 # Positional parameters:
-#   NEW_NAME (required) - the new name for the application, in either snake- or kebab-case
-#   OLD_NAME (optional) – the old name for the application, in either snake- or kebab-case
-#       Defaults to the template's short name (e.g. app-rails)
+#   APP_NAME (required) - the name for the application, use kebab-case
 # -----------------------------------------------------------------------------
 set -euo pipefail
 
 APP_NAME=$1
 CURRENT_VERSION=$(cat .template-version)
 
-# Use kebab-case
+# Enforce kebab-case
 APP_NAME_KEBAB=$(echo $APP_NAME | tr "_" "-")
 
 # Helper to get the correct sed -i behavior for both GNU sed and BSD sed (installed by default on macOS)
@@ -37,6 +35,8 @@ cp -r template-infra/infra/app infra/"$APP_NAME_KEBAB"
 
 echo "Setting up new application CI/CD..."
 cp template-infra/.github/workflows/cd-app.yml .github/workflows/"cd-$APP_NAME_KEBAB.yml"
+# This regex will capture all instances of `app` that end in a space, a double quote, a forward slash, or a hyphen
+# We do this to avoid accidentally replacing the keyword `app_name`
 LC_ALL=C sedi "s/app\([\s\"\/\-]\)/$APP_NAME_KEBAB\1/g" .github/workflows/"cd-$APP_NAME_KEBAB.yml"
 
 cp template-infra/.github/workflows/ci-app-vulnerability-scans.yml  .github/workflows/"ci-$APP_NAME_KEBAB-vulnerability-scans.yml"
