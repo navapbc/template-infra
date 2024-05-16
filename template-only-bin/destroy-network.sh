fe74152aa4fe2c625b20abb9360f131736103adf
Rocket <rocketnova@users.noreply.github.com>
2024-05-16 16:47:55 -0700

    Refactor destroy-* scripts to accept optional positional args

--- template-only-bin/destroy-network.sh
+++ template-only-bin/destroy-network.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
-# Do not use this script on your project. This script is only used for testing
-# the platform bootstrap process.
+#!/usr/bin/env bash
+# -----------------------------------------------------------------------------
+# This script destroys a network layer.
+# Do not run this script if you still have app layers deployed in this network.
+# Run this script in your project's root directory.
+#
+# Positional parameters:
+#   NETWORK_NAME (optional) - the name of the network
+#     Defaults to "dev".
+# -----------------------------------------------------------------------------
 set -euxo pipefail
 
-BACKEND_CONFIG_FILE="dev.s3.tfbackend"
+NETWORK_NAME=${1:-"dev"}
+BACKEND_CONFIG_FILE="${ENVIRONMENT_NAME}.s3.tfbackend"
 
 cd infra/networks
 
-terraform init -reconfigure -backend-config=$BACKEND_CONFIG_FILE
+terraform init -reconfigure -backend-config="${BACKEND_CONFIG_FILE}"
 
-terraform destroy -auto-approve -var="network_name=dev"
+terraform destroy -auto-approve -var="network_name=${NETWORK_NAME}"
