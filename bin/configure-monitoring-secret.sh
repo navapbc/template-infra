8cd9b0cdfce82fe2eddc264d8729e113ae77353e
Daphne Gold <daphnegold@navapbc.com>
2024-06-07 10:50:00 -0700

    Update configure-monitoring-secret.sh

--- bin/configure-monitoring-secret.sh
+++ bin/configure-monitoring-secret.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 # -----------------------------------------------------------------------------
 # This script creates SSM parameter for storing integration URL for incident management
-# services. Script creates new SSM attribute or updates existing. 
+# services. Script creates new SSM attribute or updates existing.
 #
 # Positional parameters:
-#   APP_NAME (required) – the name of subdirectory of /infra that holds the
+#   app_name (required) – the name of subdirectory of /infra that holds the
 #     application's infrastructure code.
-#   ENVIRONMENT is the name of the application environment (e.g. dev, staging, prod)
-#   INTEGRATION_ENDPOINT_URL is the url for the integration endpoint for external 
+#   environment is the name of the application environment (e.g. dev, staging, prod)
+#   integration_endpoint_url is the url for the integration endpoint for external
 #   incident management services (e.g. Pagerduty, Splunk-On-Call)
 # -----------------------------------------------------------------------------
 set -euo pipefail
 
-APP_NAME=$1
-ENVIRONMENT=$2
-INTEGRATION_ENDPOINT_URL=$3
+app_name=$1
+environment=$2
+integration_endpoint_url=$3
 
-terraform -chdir="infra/$APP_NAME/app-config" init > /dev/null
-terraform -chdir="infra/$APP_NAME/app-config" apply -auto-approve > /dev/null
+terraform -chdir="infra/$app_name/app-config" init > /dev/null
+terraform -chdir="infra/$app_name/app-config" apply -auto-approve > /dev/null
 
-HAS_INCIDENT_MANAGEMENT_SERVICE=$(terraform -chdir="infra/$APP_NAME/app-config" output -raw has_incident_management_service)
-if [ "$HAS_INCIDENT_MANAGEMENT_SERVICE" = "false" ]; then
+has_incident_management_service=$(terraform -chdir="infra/$app_name/app-config" output -raw has_incident_management_service)
+if [ "$has_incident_management_service" = "false" ]; then
   echo "Application does not have incident management service, no secret to create"
   exit 0
 fi
 
-SECRET_NAME=$(terraform -chdir="infra/$APP_NAME/app-config" output -json environment_configs | jq -r ".$ENVIRONMENT.incident_management_service_integration.integration_url_param_name")
+secret_name=$(terraform -chdir="infra/$app_name/app-config" output -json environment_configs | jq -r ".$environment.incident_management_service_integration.integration_url_param_name")
 
 echo "====================="
 echo "Setting up SSM secret"
 echo "====================="
-echo "APPLICATION_NAME=$APP_NAME"
-echo "ENVIRONMENT=$ENVIRONMENT"
-echo "INTEGRATION_URL=$INTEGRATION_ENDPOINT_URL" 
+echo "app_name=$app_name"
+echo "environment=$environment"
+echo "integration_endpoint_url=$integration_endpoint_url"
 echo
-echo "Creating SSM secret: $SECRET_NAME"
+echo "Creating SSM secret: $secret_name"
 
 aws ssm put-parameter \
-    --name "$SECRET_NAME" \
-    --value "$INTEGRATION_ENDPOINT_URL" \
+    --name "$secret_name" \
+    --value "$integration_endpoint_url" \
     --type SecureString \
     --overwrite
-
