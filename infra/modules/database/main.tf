7d3091506ddbec76929b44672f088a3f7e0f230d
Kevin Boyer <kevinboyer@navapbc.com>
2024-06-10 14:46:39 -0400

    Add documentation and tweak Terraform

--- infra/modules/database/main.tf
+++ infra/modules/database/main.tf
@@ -34,6 +34,7 @@ resource "aws_rds_cluster" "db" {
   manage_master_user_password = true
   storage_encrypted           = true
   kms_key_id                  = aws_kms_key.db.arn
+  allow_major_version_upgrade = false
 
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.rds_query_logging.name
 
@@ -76,7 +77,7 @@ resource "aws_kms_key" "db" {
 # -------------
 
 resource "aws_rds_cluster_parameter_group" "rds_query_logging" {
-  name        = var.name
+  name        = "${var.name}-${local.engine_major_version}"
   family      = "aurora-postgresql${local.engine_major_version}"
   description = "Default cluster parameter group"
 
