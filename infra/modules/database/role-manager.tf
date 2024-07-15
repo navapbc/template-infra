30c20623b7840cb561484dc6939a65699777f26c
Loren Yu <loren@navapbc.com>
2024-07-15 16:50:05 -0700

    Check in role manager dependencies (#493)
    * Check in role manager python dependencies
    * Remove local exec pip3 install step for database module
    
    ## Context
    
    A number of people were confused by the fact that the terraform plan is
    never clean due to the triggers_replace = timestamp(). There were also
    concerns about the dependency on pip3. This change adds the role manager
    dependency to the source code, which eliminates the need for the
    local-exec provisioner step and also removes the dependency on pip3. The
    cost is that there is now some vendor code in the
    /modules/database/role_manager/vendor folder.

--- infra/modules/database/role-manager.tf
+++ infra/modules/database/role-manager.tf
@@ -53,25 +53,10 @@ resource "aws_lambda_function" "role_manager" {
   # checkov:skip=CKV_AWS_116:Dead letter queue (DLQ) configuration is only relevant for asynchronous invocations
 }
 
-# Installs python packages needed by the role manager lambda function before
-# creating the zip archive. 
-# Runs pip install on every apply so that the role manager archive file that
-# is generated locally is guaranteed to have the required dependencies even
-# when terraform is run by a developer that did not originally create the
-# environment.
-# Timestamp is used to always trigger replacement.
-resource "terraform_data" "role_manager_python_vendor_packages" {
-  triggers_replace = fileexists(local.role_manager_package) ? file("${path.module}/role_manager/requirements.txt") : timestamp()
-  provisioner "local-exec" {
-    command = "pip3 install -r ${path.module}/role_manager/requirements.txt -t ${path.module}/role_manager/vendor --upgrade"
-  }
-}
-
 data "archive_file" "role_manager" {
   type        = "zip"
   source_dir  = "${path.module}/role_manager"
   output_path = local.role_manager_package
-  depends_on  = [terraform_data.role_manager_python_vendor_packages]
 }
 
 data "aws_kms_key" "default_ssm_key" {
