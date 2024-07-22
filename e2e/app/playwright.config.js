cc79aa419c1d1d36ffad90e4a736738f554fba3d
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-22 15:09:14 -0700

    add deepMerge for playwright configs

--- e2e/app/playwright.config.js
+++ e2e/app/playwright.config.js
@@ -2,10 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 import baseConfig from '../playwright.config';
 
-export default defineConfig({
-  ...baseConfig,
-  use: {
-    ...baseConfig.use,
-    baseUrl: baseConfig.use.baseUrl || "localhost:3000"
-  },
-});
+export default defineConfig(deepMerge(
+    baseConfig,
+    {
+      use: {
+        baseUrl: baseConfig.use.baseUrl || "localhost:3000"
+      },
+    }
+  ));  
