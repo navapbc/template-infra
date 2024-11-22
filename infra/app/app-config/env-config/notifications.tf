b38a5f23b56d950c2a0a411fe93105775f6d6dac
kai [they] <coilysiren@gmail.com>
2024-11-22 12:49:28 -0800

    Update notifications.tf

--- infra/app/app-config/env-config/notifications.tf
+++ infra/app/app-config/env-config/notifications.tf
@@ -10,7 +10,7 @@ locals {
 
     # Set to the email address to be used when sending emails.
     # If enable_notifications is true, this is required.
-  sender_email = "notifications@${var.domain_name}"
+    sender_email = "notifications@${var.domain_name}"
 
     # Configure the REPLY-TO email address if it should be different from the sender.
     reply_to_email = "notifications@${var.domain_name}"
