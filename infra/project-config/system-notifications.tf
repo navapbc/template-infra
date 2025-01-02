2cf0b8bd2d045ae3c5ec8627f41325c6ee38806a
Loren Yu <loren@navapbc.com>
2025-01-02 15:45:32 -0800

    Add workflow that scans orphaned PR environments

--- infra/project-config/system-notifications.tf
+++ infra/project-config/system-notifications.tf
@@ -7,10 +7,11 @@ locals {
   system_notifications_config = {
     channels = {
       workflow-failures = {
-        "type" = "slack" # or "teams"
-        # Name of the secret in GitHub
-        "channel_id_secret_name"  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
-        "slack_token_secret_name" = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
+        # Uncomment if you want to send notifications to a Slack channel
+        # "type" = "slack"
+        # # Name of the secret in GitHub
+        # "channel_id_secret_name"  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
+        # "slack_token_secret_name" = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
       }
     }
   }
