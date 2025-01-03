a887ff2013a44c513eeace428b11231daee2beb2
Loren Yu <loren@navapbc.com>
2025-01-03 10:27:16 -0800

    Improve comment readability

--- infra/project-config/system-notifications.tf
+++ infra/project-config/system-notifications.tf
@@ -5,22 +5,31 @@ locals {
   # failed workflows, etc.
   system_notifications_config = {
 
-    # channels is a map of notification channels, where each key is a
-    # notification channel and each value is the channel configuration.
+    # The `channels` map defines notification channels. Each key represents a
+    # notification channel, and each value is the channel's configuration.
     #
-    # A channel configuration contains contains the following attributes:
-    #   type: Type of notification channel (e.g. "slack", or "teams").
-    #         currently only "slack" is supported
+    # Each channel configuration includes the following attributes:
+    #   - type: The type of notification channel (e.g., "slack" or "teams").
+    #           Currently, only "slack" is supported.
     #
-    # If the channel `type` is missing or null, system notifications sent to
-    # that channel will result in a no-op.
+    # If the `type` attribute is missing or null, notifications sent to that
+    # channel will be ignored (no-op).
     #
-    # If the channel `type` is "slack", the configuration must also contain the
-    # following attributes:
-    #   channel_id_secret_name: Name of the secret in GitHub that contains the
-    #                           Slack channel ID
-    #   slack_token_secret_name: Name of the secret in GitHub that contains the
-    #                           Slack bot token
+    # For channels with `type` set to "slack", the configuration must also
+    # include the following attributes:
+    #   - channel_id_secret_name: The name of the secret in GitHub that contains
+    #                             the Slack channel ID.
+    #   - slack_token_secret_name: The name of the secret in GitHub that contains
+    #                              the Slack bot token.
+    #
+    # Example:
+    # channels = {
+    #   alerts = {
+    #     type                    = "slack"
+    #     channel_id_secret_name  = "SYSTEM_NOTIFICATIONS_SLACK_CHANNEL_ID"
+    #     slack_token_secret_name = "SYSTEM_NOTIFICATIONS_SLACK_BOT_TOKEN"
+    #   }
+    # }
     channels = {
       workflow-failures = {
         # Uncomment if you want to send workflow failure notifications to Slack
