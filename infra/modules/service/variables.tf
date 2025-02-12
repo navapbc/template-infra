60934c7e40f598c0b4f737891d39d955a6c3eafc
Tanner Doshier <git@doshitan.com>
2025-02-12 13:23:05 -0500

    Support ephemeral write mounts for services (#871)
    Fargate tasks come with 20 GiB of ephemeral storage[1] by default (the container
    image size itself eats into this) and can be increased up to a maximum of 200
    GiB.
    
    While generally encouraged to store files/write data to a network location/a
    database, there are situations where it is useful to have some "local" file
    space to write data to (e.g., tmpfiles, processing a large data file).
    
    Fargate can be configured to mount its allocated ephemeral space into the
    container to facilitate this. This has a few pieces[2], in summary:
    
    1. Create the desired writable locations in the container image, with the
       appropriate permissions/owner, and declare them with a `VOLUME` instruction:
    
        ```dockerfile
        VOLUME /var/scratch
        ```
    2. In the task definition `volumes` section, define a bind mount:
    
        ```json
        "volumes": [
            {
                "name": "scratch_space"
            }
        ]
        ```
        (the name doesn't really matter, but is limited to 255 characters of a limited set[3])
    3. For the appropriate entries in the `containerDefintions` part of the task
       definition, define a `mountPoints` entry connecting the volume name from step
       2, with the `VOLUME` path from step 1:
    
        ```json
        "containerDefinitions": [
            {
              "name": "my-app",
              "image": "my-repo/my-app",
              "cpu": 100,
              "memory": 100,
              "essential": true,
              "mountPoints": [
                  {
                     "sourceVolume": "scratch_space",
                     "containerPath": "/var/scratch",
                     "readOnly": false
                  }
              ]
            }
        ]
        ```
    
    This will result in a mounted location in the container, with as much space as
    is free in the allocated ephemeral storage of the Fargate task, whose contents
    are thrown away when the task stops.
    
    [1] https://docs.aws.amazon.com/AmazonECS/latest/developerguide/fargate-task-storage.html#fargate-task-storage-linux-pv
    [2] https://docs.aws.amazon.com/AmazonECS/latest/developerguide/bind-mount-examples.html#:~:text=To%20provide%20an%20empty%20data%20volume%20for%20one%20or%20more%20containers
    [3] https://docs.aws.amazon.com/AmazonECS/latest/developerguide/specify-bind-mount-config.html
    
    Related to https://github.com/navapbc/template-application-rails/issues/58

--- infra/modules/service/variables.tf
+++ infra/modules/service/variables.tf
@@ -169,3 +169,9 @@ variable "vpc_id" {
   type        = string
   description = "Uniquely identifies the VPC."
 }
+
+variable "ephemeral_write_volumes" {
+  type        = set(string)
+  description = "A set of absolute paths in the container to be mounted as writable for the life of the task. These need to be declared with `VOLUME` instructions in the container build file."
+  default     = []
+}
