ff13ffcd9639202633b882b7eaa997cf6ea0ebcd
Ryan Lewis <ryanlewis@navapbc.com>
2025-02-13 19:07:14 -0800

    use standard prettier config

--- e2e/{{app_name}}/tests/index.spec.js
+++ e2e/{{app_name}}/tests/index.spec.js
@@ -15,9 +15,7 @@ test.describe('Generic Webpage Tests', () => {
   });
 
   // https://playwright.dev/docs/accessibility-testing
-  test('should not have any automatically detectable accessibility issues', async ({
-    page,
-  }) => {
+  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('/');
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
