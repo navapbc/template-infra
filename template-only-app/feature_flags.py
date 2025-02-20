b71bdc738ddcd4d1a03052803f43c2f95a325b15
Loren Yu <loren@navapbc.com>
2025-02-19 16:01:13 -0800

    Remove unused imports
    Co-authored-by: Tanner Doshier <git@doshitan.com>

--- template-only-app/feature_flags.py
+++ template-only-app/feature_flags.py
@@ -1,8 +1,5 @@
-import logging
 import os
-import boto3
-logger = logging.getLogger()
+
 def is_feature_enabled(feature_name: str) -> bool:
     value = os.environ.get(f"FF_{feature_name}")
     return value == "true" if value else False
-
