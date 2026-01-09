6f46a1d56d6f5b517da5b319c84cc69fb7cf156c
Laurence Goolsby <laurencegoolsby@navapbc.com>
2026-01-09 15:36:13 -0500

    Rename dde_profile_arn to bda_profile_arn

--- infra/{{app_name}}/service/document_data_extraction.tf
+++ infra/{{app_name}}/service/document_data_extraction.tf
@@ -6,7 +6,7 @@ locals {
     DDE_INPUT_LOCATION  = "${local.prefix}${local.document_data_extraction_config.input_bucket_name}"
     DDE_OUTPUT_LOCATION = "${local.prefix}${local.document_data_extraction_config.output_bucket_name}"
     DDE_PROJECT_ARN     = module.dde[0].bda_project_arn
-    DDE_PROFILE_ARN     = module.dde[0].dde_profile_arn
+    DDE_PROFILE_ARN     = module.dde[0].bda_profile_arn
   } : {}
 }
 
