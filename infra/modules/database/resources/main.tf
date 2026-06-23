bdcc2e3491bcd86d257de919acfca4adea5a9a09
Tanner Doshier <git@doshitan.com>
2026-06-23 12:15:35 -0400

    Add skip referencing ticket for CKV_AWS_353

--- infra/modules/database/resources/main.tf
+++ infra/modules/database/resources/main.tf
@@ -83,6 +83,8 @@ resource "aws_rds_cluster_instance" "primary" {
   # window, but when you want changes to happen now, set this.
   #
   # apply_immediately = true
+
+  # checkov:skip=CKV_AWS_353:TODO(https://github.com/navapbc/template-infra/issues/1091) sort out the performance/database insights feature
 }
 
 resource "aws_kms_key" "db" {
